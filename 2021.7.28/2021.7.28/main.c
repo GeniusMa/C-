a891369e096836ba168df119095cd33a975f9ac4
GeniusMa <1916986256@qq.com>
2021-07-28 09:00:40 +0800

    2

---
+++ 2021.7.28/2021.7.28/main.c
@@ -0,0 +1,67 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+//int main() 
+//{
+//	int a = 10;
+//	float f = 10.0;
+//	short b = 10;//short 后面写不写INT都可以
+//	//unsigned无符号
+//	//signed  有符号
+//	return 0;
+//}
+
+//类型的意义
+//1.使用这个类型开辟内存空间的大小
+//2.如何看待内存空间的视角（不同类型数据存储格式不同）
+
+/*
+自定义类型（构造类型）
+1.数组类型
+2.结构体类型 struct
+3.枚举类型 enum
+4.联合类型 union
+*/
+
+/*
+void 空类型（无类型）
+一般用于无返回值的函数
+*/
+
+//void test() 
+//{
+//	printf("olofmeister");
+//}
+//
+//int main() 
+//{
+//	test();
+//	return 0;
+//}
+
+int main() 
+{
+
+	return 0;
+}
+
+
+/*
+大端存储模式  
+小端存储模式  
+*/
+
+
+
+
+
+
+
+
+
+/************************
+		 马海淇
+	   o1ofmeister
+*************************/
