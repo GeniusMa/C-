41e777d5399c92947030f3065ce2ef51604e610c
GeniusMa <1916986256@qq.com>
2021-07-30 13:57:32 +0800

    2021.7.30偷懒

---
+++ 2021.7.30/2021.7.30/main.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+//int main() 
+//{
+//	char a[1000];
+//	int i = 0;
+//	for (i = 0; i < 1000; i++) 
+//	{
+//		a[i] = -1 - i;
+//
+//	}
+//	printf("%d\n", strlen(a));
+//	return 0;
+//}
+int main()
+{
+	unsigned char a = 0;
+	for (a = 0; a <= 255; a++) 
+	{
+		printf("olofmeister\n");
+	}
+	return 0;
+}
