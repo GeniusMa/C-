4a0bdd2efc74a76f14c56f25739d8df054588a1a
小马 <87123120+GeniusMa@users.noreply.github.com>
2021-10-12 13:07:42 +0800

    10.11

---
+++ 2021.10.11/main.c
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+
+//struct S 
+//{
+//	int a;
+//	int b;
+//};
+//
+//int main() 
+//{
+//	struct S a;
+//	struct S *p = &a;
+//	p->a = 6;
+//	printf("%d\n", a.a);
+//	printf("%d\n", p->a);
+//	printf("%d\n", (*p).a);
+//	//printf("%d\n", *p.a);
+//
+//	return 0;
+//}
+
+
+//int main() 
+//{
+//	int money = 0;
+//	int total = 0;
+//	int empty = 0;
+//	scanf("%d", &money);
+//	total = money;
+//	empty = money;
+//
+//	while (empty >= 2) 
+//	{
+//		total += empty / 2;
+//		empty = empty / 2 + empty % 2;
+//	}
+//	printf("%d\n", total);
+//	return 0;
+//}
+
+int mian() 
+{
+	
+	return 0;
+}
