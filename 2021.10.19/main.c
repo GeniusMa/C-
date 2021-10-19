9216367c37cbb542e7ea989462cf1342ac9b62aa
小马 <87123120+GeniusMa@users.noreply.github.com>
2021-10-19 22:16:32 +0800

    10.19

---
+++ 2021.10.19/main.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+
+//int main()
+//{
+//    char arr[] = { "olof.meister@top1" };
+//    char* p = { ".@" };
+//    //char* ret = strtok(arr, p);
+//    //printf("%s\n", ret);
+//    //ret = strtok(NULL, p);
+//    //printf("%s\n", ret);
+//    //ret = strtok(NULL, p);
+//    //printf("%s\n\n", ret);
+//    char* ret = NULL;
+//    for (ret = strtok(arr, p); ret != NULL; ret = strtok(NULL, p)) 
+//    {
+//        printf("%s\n", ret);
+//    }
+//    return 0;
+//}
+
+//int main()
+//{
+//	// 打开文件
+//	FILE* pf = fopen("text.txt", "r");
+//	if (pf == NULL) 
+//	{
+//		printf("%s\n", strerror(errno));
+//	}
+//	else 
+//	{
+//		printf("oprn file sucess\n");
+//	}
+//	return 0;
+//}
+
+int main() 
+{
+
+	return 0;
+}
